22fe3e28daf42d3b70cbdab6832da7bf22ef7c98
ssa <storogh.sa@yandex.ru>
2020-09-08 12:52:36 +0400

    -

--- app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
@@ -2,9 +2,10 @@ package ru.skillbranch.skillarticles.ui
 
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.Toolbar
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_root.*
